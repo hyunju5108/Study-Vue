a1b070c007b4ca1eaafc435c5fe464d6de25123d
나현주 <hyunju5108@naver.com>
2022-12-05 09:12:33 +0900

    [Vue NewsProject] UserProfile Component Commonization

--- vue-news/src/views/ItemView.vue
+++ vue-news/src/views/ItemView.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
     <section>
-      <!-- 질문 상세 정보 -->
-      <div class="user-container">
+      <!-- 사용자 정보 -->
+      <user-profile :info="fetchedItem">
+        <!-- <div slot="username"></div> -->
+        <router-link slot="username" :to="`/user/${fetchedItem.user}`">
+          {{ fetchedItem.user }}
+        </router-link>
+        <template slot="time">{{ 'Posted '+fetchedItem.time_ago }}</template>
+      </user-profile>
+      <!-- <div class="user-container">
         <div>
           <i class="fas fa-user"></i>
         </div>
@@ -14,9 +21,10 @@
             {{ fetchedItem.time_ago }}
           </div>
         </div>
-      </div>
+      </div> -->
+    </section>
+    <section>
       <h2>{{ fetchedItem.title }}</h2>
-
     </section>
     <section>
       <!-- 질문 댓글 -->
@@ -30,9 +38,13 @@
 </template>
 
 <script>
+import UserProfile from '../components/UserProfile.vue';
 import {mapGetters} from 'vuex';
 
 export default {
+  components : {
+    UserProfile
+  },
   created() {
     console.log(this.$store.state.item);
 
--- vue-news/src/views/UserView.vue
+++ vue-news/src/views/UserView.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
-    <p>name : {{ userInfo.id }}</p>
+    <user-profile :info="userInfo">
+      <div slot="username">{{ userInfo.id }}</div>
+      <span slot="time">{{ 'Joined ' + userInfo.created }}, </span>
+      <span slot="karma">{{ userInfo.karma }}</span>
+    </user-profile>
+    <!-- <p>name : {{ userInfo.id }}</p>
     <p>karma : {{ userInfo.karma }}</p>
-    <p>created : {{ userInfo.created }}</p>
+    <p>created : {{ userInfo.created }}</p> -->
   </div>
 </template>
 
 <script>
+import UserProfile from '../components/UserProfile.vue'
+
 export default {
+  components: {
+    UserProfile,
+  },
   computed: {
     userInfo() {
       return this.$store.state.user;
